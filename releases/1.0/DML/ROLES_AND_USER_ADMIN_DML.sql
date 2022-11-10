da260ec1ad077828150659fb4ad52662d6622797
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2022-11-09 21:11:39 -0500

    Create ROLES_AND_USER_ADMIN_DML.sql

---
+++ releases/1.0/DML/ROLES_AND_USER_ADMIN_DML.sql
@@ -0,0 +1,7 @@
+INSERT INTO public."ROLES" (id, "name") VALUES(1, 'ROLE_ADMIN');
+INSERT INTO public."ROLES" (id, "name") VALUES(2, 'ROLE_USER');
+
+INSERT INTO public."USERS" (id, username, "password", enabled, "name", last_name, email, attempts) VALUES(1, 'pedro.chavez', '$2a$10$Rk007.3NbjwrSEHaM36abO/2NqI6gbwmwcXaCTzf5bbnr8WpZtVPa', true, 'Pedro Luis', 'Chavez Castro', 'pedro.chavezcastro@outlook.com', 0);
+
+INSERT INTO public."USERS_ROLES" (id, id_user_fk, id_role_fk) VALUES(1, 1, 1);
+INSERT INTO public."USERS_ROLES" (id, id_user_fk, id_role_fk) VALUES(1, 1, 1);
